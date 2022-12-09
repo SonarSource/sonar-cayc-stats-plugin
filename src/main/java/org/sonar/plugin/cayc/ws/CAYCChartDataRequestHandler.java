7f2bb64f34dc5f3bd9bde89fe4e11134ed3ad2d7
Dimitris Kavvathas <dimitris.kavvathas@sonarsource.com>
2022-12-09 16:36:19 +0100

    Add issues histogram data at the cayc/issues_creation_histogram endpoint

--- src/main/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandler.java
+++ src/main/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandler.java
@@ -1,16 +1,23 @@
 package org.sonar.plugin.cayc.ws;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.RequestHandler;
 import org.sonar.api.server.ws.Response;
 import org.sonar.api.utils.text.JsonWriter;
-import org.sonarqube.ws.Issues;
+import org.sonar.plugin.cayc.ws.exceptions.InvalidRequestException;
+import org.sonarqube.ws.Common.FacetValue;
 import org.sonarqube.ws.client.WsClient;
 import org.sonarqube.ws.client.WsClientFactories;
 import org.sonarqube.ws.client.issues.SearchRequest;
 
-public class CAYCChartDataRequestHandler implements RequestHandler {
+import static org.sonar.plugin.cayc.ws.CAYCWebService.DEFAULT_YEARS;
 
+public class CAYCChartDataRequestHandler implements RequestHandler {
   private WsClient wsClient;
 
   public CAYCChartDataRequestHandler() {
@@ -23,17 +30,86 @@ public class CAYCChartDataRequestHandler implements RequestHandler {
   @Override
   public void handle(Request request, Response response) {
     initialiseWsClient(request);
-    Issues.SearchWsResponse issuesResponse = wsClient.issues().search(new SearchRequest());
-    try (JsonWriter json = response.newJsonWriter()) {
-      json
-        .value(issuesResponse.toString())
-        .close();
+
+    try {
+      writeResponse(response, issuesCreatedHistogram(getYears(request)));
+    } catch (InvalidRequestException e) {
+      try (JsonWriter json = response.newJsonWriter()) {
+        json.beginObject()
+          .prop("msg", e.getMessage())
+          .endObject()
+          .close();
+      }
     }
+  }
+
+  private List<FacetValue> issuesCreatedHistogram(int years) {
+    List<LocalDate> yearDates = new ArrayList<>();
+    for (int year = years - 1; year >= 0; year--) {
+      yearDates.add(getDateMinusYear(year));
+    }
+
+    return yearDates.stream()
+      .map(this::yearlyCreatedIssuesSearchRequest)
+      .map(request -> wsClient.issues().search(request))
+      .flatMap(response -> response.getFacets().getFacets(0).getValuesList().stream())
+      .collect(Collectors.toList());
+  }
+
+  private static LocalDate getDateMinusYear(int year) {
+    LocalDate currentYear = LocalDate.now();
+    return currentYear.minusYears(year);
+  }
+
+  private static int getYears(Request request) throws InvalidRequestException {
+    String years = request.param("years");
+    if (years != null) {
+      try {
+        int yearsNumber = Integer.parseInt(years);
+        if (yearsNumber <= 0) {
+          throw new InvalidRequestException("Invalid \"years\" parameter: " + years + ". Should be a positive number");
+        }
+        return yearsNumber;
+      } catch (NumberFormatException e) {
+        throw new InvalidRequestException("Invalid \"years\" parameter: " + years, e);
+      }
+    }
+    return DEFAULT_YEARS;
+  }
+
+  private SearchRequest yearlyCreatedIssuesSearchRequest(LocalDate date) {
+    return new SearchRequest()
+      .setCreatedAfter(isoFormat(date.minusYears(1)))
+      .setCreatedBefore(isoFormat(date))
+      .setFacets(List.of("createdAt"));
+  }
+
+  private String isoFormat(LocalDate date) {
+    return date.format(DateTimeFormatter.ISO_DATE);
   }
 
   private void initialiseWsClient(Request request) {
     if (wsClient == null) {
       wsClient = WsClientFactories.getLocal().newClient(request.localConnector());
+    }
+  }
+
+  private static void writeResponse(Response response, List<FacetValue> facetValues) {
+    try (JsonWriter json = response.newJsonWriter()) {
+      json.beginObject()
+        .name("history")
+        .beginArray();
+
+      facetValues.forEach(
+        v -> json.beginObject()
+          .prop("date", v.getVal())
+          .prop("value", v.getCount())
+          .endObject()
+      );
+
+      json.endArray()
+        .endObject()
+        .close();
     }
   }
 }
--- src/test/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandlerTest.java
+++ src/test/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandlerTest.java
@@ -1,17 +1,22 @@
 package org.sonar.plugin.cayc.ws;
 
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
+import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
-import org.sonarqube.ws.Issues;
+import org.sonarqube.ws.Common.Facet;
+import org.sonarqube.ws.Common.FacetValue;
+import org.sonarqube.ws.Common.Facets;
 import org.sonarqube.ws.Issues.SearchWsResponse;
 import org.sonarqube.ws.client.WsClient;
 import org.sonarqube.ws.client.issues.IssuesService;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.sonar.test.JsonAssert.assertJson;
 
 public class CAYCChartDataRequestHandlerTest {
 
@@ -26,15 +31,78 @@ public class CAYCChartDataRequestHandlerTest {
   }
 
   @Test
-  public void noHistoryBeforeSQ() {
-    var request = new MockChartDataRequest();
+  public void defaultYearParameter() {
+    var request = mock(Request.class);
 
-    var issueServices = mock(IssuesService.class);
-    doReturn(issueServices).when(wsClientMock).issues();
-    SearchWsResponse searchWsResponse = SearchWsResponse.newBuilder().addIssues(Issues.Issue.newBuilder().setStatus("open")).build();
-    doReturn(searchWsResponse).when(issueServices).search(any());
+    mockIssuesSearch();
     underTest.handle(request, response);
 
-    assertEquals("\"issues {\\n  status: \\\"open\\\"\\n}\\n\"", ((DumbResponse) response).output.toString());
+    assertJson(((DumbResponse) response).output.toString())
+
+      .isSimilarTo(this.getClass().getResource("issues_histogram_8_years.json"));
+  }
+
+  @Test
+  public void yearParameter() {
+    var request = mock(Request.class);
+    doReturn("1").when(request).param("years");
+
+    mockIssuesSearch();
+    underTest.handle(request, response);
+
+    assertJson(((DumbResponse) response).output.toString())
+      .isSimilarTo(this.getClass().getResource("issues_histogram_1_year.json"));
+  }
+
+  @Test
+  public void givenAnInvalidYearsParamWhenHandlingRequestThenAnErrorMessageShouldBeReturned() {
+    var request = mock(Request.class);
+    doReturn("test").when(request).param("years");
+    mockIssuesSearch();
+    underTest.handle(request, response);
+
+    assertJson(((DumbResponse) response).output.toString())
+      .isSimilarTo("{\"msg\":\"Invalid \\\"years\\\" parameter: test\"}");
+  }
+
+  private void mockIssuesSearch() {
+    var issueServices = mock(IssuesService.class);
+    doReturn(issueServices).when(wsClientMock).issues();
+
+    Facet facet = mock(Facet.class);
+    List<FacetValue> facetValues = mockYearlyFacetValues("2022");
+    doReturn(facetValues).when(facet).getValuesList();
+
+    Facets facets = mock(Facets.class);
+    doReturn(facet).when(facets).getFacets(anyInt());
+
+    SearchWsResponse searchWsResponse = mock(SearchWsResponse.class);
+    doReturn(facets).when(searchWsResponse).getFacets();
+
+    doReturn(searchWsResponse).when(issueServices).search(any());
+  }
+
+  private List<FacetValue> mockYearlyFacetValues(String year) {
+    return List.of(
+      mockFacetValue(year + "-01-01", 52),
+      mockFacetValue(year + "-02-01", 100),
+      mockFacetValue(year + "-03-01", 200),
+      mockFacetValue(year + "-04-01", 250),
+      mockFacetValue(year + "-05-01", 260),
+      mockFacetValue(year + "-06-01", 200),
+      mockFacetValue(year + "-07-01", 450),
+      mockFacetValue(year + "-08-01", 460),
+      mockFacetValue(year + "-09-01", 500),
+      mockFacetValue(year + "-10-01", 520),
+      mockFacetValue(year + "-11-01", 500),
+      mockFacetValue(year + "-12-01", 560)
+    );
+  }
+
+  private FacetValue mockFacetValue(String date, long issues) {
+    FacetValue facetValue = mock(FacetValue.class);
+    doReturn(date).when(facetValue).getVal();
+    doReturn(issues).when(facetValue).getCount();
+    return facetValue;
   }
 }
