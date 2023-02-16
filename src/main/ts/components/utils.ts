1c20c4717ab00bbce39205395da6b9c5b7286472
Ambroise C <ambroise.christea@sonarsource.com>
2023-02-16 15:02:46 +0100

    [WAITING FOR JIRA] Update CaYC projection curve computing

---
+++ src/main/ts/components/utils.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2022-2022 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+import { subMonths } from "date-fns";
+import { CAYC_PERIOD_IN_MONTHS } from "../constants";
+
+export function computeCaycStartingDate() {
+  return subMonths(new Date(), CAYC_PERIOD_IN_MONTHS)
+}
