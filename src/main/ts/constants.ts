9119e97dfdabc2e1b1aaa7df20f8b8608aa151cb
Ambroise C <ambroise.christea@sonarsource.com>
2023-02-17 17:22:05 +0100

    CAYCPLUGIN-4 Add label for graph's origin point
    - Improve display of text about issues burn with CaYC
    - Add border around graph

--- src/main/ts/constants.ts
+++ src/main/ts/constants.ts
@@ -5,6 +5,7 @@
  */
 export const GRAPH_HEIGHT = 300;
 export const GRAPH_WIDTH = 500;
-export const GRAPH_PADDING = 20;
+export const GRAPH_VERTICAL_MARKER_Y_POSITION_OFFSET = -20;
+export const GRAPH_VERTICAL_MARKER_DATE_FORMAT = 'MMM yyyy';
 
 export const CAYC_PERIOD_IN_MONTHS = 24;
