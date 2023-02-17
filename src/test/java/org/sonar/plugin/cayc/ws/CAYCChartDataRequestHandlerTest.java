bdf106e579b7b360ec1388611017f0a28d3a2fc5
Zipeng WU <zipeng.wu@sonarsource.com>
2023-02-17 17:49:05 +0100

    CAYCPLUGIN-1 API return accurate histogram data

--- src/test/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandlerTest.java
+++ src/test/java/org/sonar/plugin/cayc/ws/CAYCChartDataRequestHandlerTest.java
@@ -5,9 +5,17 @@
  */
 package org.sonar.plugin.cayc.ws;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.stubbing.OngoingStubbing;
 import org.sonar.api.server.ws.Request;
 import org.sonar.api.server.ws.Response;
 import org.sonarqube.ws.Common.Facet;
@@ -16,78 +24,97 @@ import org.sonarqube.ws.Common.Facets;
 import org.sonarqube.ws.Issues.SearchWsResponse;
 import org.sonarqube.ws.client.WsClient;
 import org.sonarqube.ws.client.issues.IssuesService;
+import org.sonarqube.ws.client.issues.SearchRequest;
 
+import static java.time.format.DateTimeFormatter.ISO_DATE;
+import static java.util.stream.Collectors.toSet;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.sonar.api.rules.RuleType.BUG;
+import static org.sonar.api.rules.RuleType.VULNERABILITY;
 import static org.sonar.test.JsonAssert.assertJson;
 
 public class CAYCChartDataRequestHandlerTest {
 
   private CAYCChartDataRequestHandler underTest;
   private WsClient wsClientMock = mock(WsClient.class);
+  private IssuesService issueService = mock(IssuesService.class);
   private Response response;
 
   @Before
   public void setup() {
     underTest = new CAYCChartDataRequestHandler(wsClientMock);
     response = new DumbResponse();
+    doReturn(issueService).when(wsClientMock).issues();
   }
 
   @Test
-  public void defaultYearParameter() {
+  public void request_last_15_years_issue_history() {
+    var request = mock(Request.class);
+    mockIssuesSearch();
+    ArgumentCaptor<SearchRequest> searchCaptor = ArgumentCaptor.forClass(SearchRequest.class);
+
+    underTest.handle(request, response);
+
+    verify(issueService, times(15)).search(searchCaptor.capture());
+
+    var searchRequests = searchCaptor.getAllValues();
+
+    Set<String> searchYears = IntStream.range(0, 15).boxed().map(i -> LocalDate.now().minusYears(i).format(ISO_DATE)).collect(toSet());
+
+    for (var searchRequest : searchRequests) {
+      assertThat(searchYears.remove(searchRequest.getCreatedBefore())).isTrue();
+      assertThat(searchRequest.getTypes()).containsExactly(BUG.name(), VULNERABILITY.name());
+      assertThat(searchRequest.getResolved()).isEqualTo("false");
+      assertThat(searchRequest.getFacets()).containsExactly("createdAt");
+    }
+
+    assertThat(searchYears).isEmpty();
+
+  }
+
+  @Test
+  public void verify_json_output() {
     var request = mock(Request.class);
 
     mockIssuesSearch();
     underTest.handle(request, response);
 
     assertJson(((DumbResponse) response).output.toString())
-
-      .isSimilarTo(this.getClass().getResource("issues_histogram_8_years.json"));
-  }
-
-  @Test
-  public void yearParameter() {
-    var request = mock(Request.class);
-    doReturn("1").when(request).param("years");
-
-    mockIssuesSearch();
-    underTest.handle(request, response);
-
-    assertJson(((DumbResponse) response).output.toString())
-      .isSimilarTo(this.getClass().getResource("issues_histogram_1_year.json"));
-  }
-
-  @Test
-  public void givenAnInvalidYearsParamWhenHandlingRequestThenAnErrorMessageShouldBeReturned() {
-    var request = mock(Request.class);
-    doReturn("test").when(request).param("years");
-    mockIssuesSearch();
-    underTest.handle(request, response);
-
-    assertJson(((DumbResponse) response).output.toString())
-      .isSimilarTo("{\"msg\":\"Invalid \\\"years\\\" parameter: test\"}");
+      .isSimilarTo(this.getClass().getResource("issues_histogram.json"));
   }
 
   private void mockIssuesSearch() {
-    var issueServices = mock(IssuesService.class);
-    doReturn(issueServices).when(wsClientMock).issues();
 
     Facet facet = mock(Facet.class);
-    List<FacetValue> facetValues = mockYearlyFacetValues("2022");
-    doReturn(facetValues).when(facet).getValuesList();
+    var searchResults = IntStream.range(0, 15)
+      .map(i -> 2022 - i)
+      .boxed()
+      .map(this::mockYearlyFacetValues)
+      .collect(Collectors.toList());
+
+    var stubbing = when(facet.getValuesList());
+    for (var result : searchResults) {
+      stubbing = stubbing.thenReturn(result);
+    }
 
     Facets facets = mock(Facets.class);
     doReturn(facet).when(facets).getFacets(anyInt());
 
     SearchWsResponse searchWsResponse = mock(SearchWsResponse.class);
+    doReturn(12L).when(searchWsResponse).getTotal();
     doReturn(facets).when(searchWsResponse).getFacets();
 
-    doReturn(searchWsResponse).when(issueServices).search(any());
+    doReturn(searchWsResponse).when(issueService).search(any());
   }
 
-  private List<FacetValue> mockYearlyFacetValues(String year) {
+  private List<FacetValue> mockYearlyFacetValues(int year) {
     return List.of(
       mockFacetValue(year + "-01-01", 52),
       mockFacetValue(year + "-02-01", 100),
