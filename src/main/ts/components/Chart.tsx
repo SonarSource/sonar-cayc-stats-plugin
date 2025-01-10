1a1acf1814da0075237695f98ff6a10416d35ed1
Jacek Poreda <jacek.poreda@sonarsource.com>
2025-01-10 14:35:04 +0100

    CAYCPLUGIN-52 Update license headers

--- src/main/ts/components/Chart.tsx
+++ src/main/ts/components/Chart.tsx
@@ -1,6 +1,6 @@
 /*
  * Clean as You Code Plugin
- * Copyright (C) 2022-2024 SonarSource SA
+ * Copyright (C) 2022-2025 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
